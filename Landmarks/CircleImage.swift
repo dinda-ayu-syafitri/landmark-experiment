8ea31b61200a591595ee2fe3258209206970dd91
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-03-12 11:13:05 +0700

    creating and combining views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Dinda Ayu Syafitri on 12/03/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dinda Ayu Syafitri on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
