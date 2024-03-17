9515aa3cbab3da0c8cd21fe89f9d76a8bde51dd3
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-03-17 22:04:19 +0700

    Add : Set Up navigation between list and detail

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Dinda Ayu Syafitri on 17/03/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView().frame(height: 300)
+            
+            CircleImage().offset(y:-130)
+                .padding(.bottom,-130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundStyle(Color(red:51/255, green:138/255, blue:4/255))
+                .bold()
+                HStack {
+                    Text("Joshua Tree National Park")
+                        
+                    Spacer()
+                    Text("California").font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(Color.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Some Description AAAAAAAAA")
+            }
+            .padding()
+            
+            Spacer()
+        }    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
