341bd900ac10d807f17a6b0ab09168424181e182
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-03-17 21:27:30 +0700

    Add: Row Preview Costumization

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -20,5 +20,9 @@ struct LandmarkRow: View {
 }
 
 #Preview {
-    LandmarkRow(landmark: landmarks[0])
+    Group{
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
+
